c7c4acdf152dce6c776897bf3a19dc99e1e80ca1
John DeVries <john.devries@educate360.com>
2025-08-26 07:29:00 -0700

    ascsfds

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿//A new comment
-namespace Assignment1._1._1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string name = "John DeVries";
+int age = 45;
+string address = "123 Main St. Sequim WA 98382";
+
+Console.WriteLine($"User's name is: {name}");
+Console.WriteLine($"User's age is: {age}");
+Console.WriteLine($"User's address is: {address}");
